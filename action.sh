047149d11215414bd90a7a771d2aa3fa513d38ae
ghete3 <131985891+ghete3@users.noreply.github.com>
2023-04-27 12:20:36 -0400

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
-git clone -b $branch https://github.com/ghete3/Ultroid /root/ghete3
-cp ultroid/.env /root/ghete3/.env
-cd /root/ghete3
+git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
+cp ultroid/.env /root/TeamUltroid/.env
+cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
